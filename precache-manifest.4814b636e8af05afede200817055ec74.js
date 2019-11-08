1135f694a53fcec0170ec477fb01b7b59796eec5
kine1991 <kine4599@gmail.com>
2019-11-08 17:00:51 +0300

    Updates

---
+++ precache-manifest.4814b636e8af05afede200817055ec74.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18bbe7d764ddf6c8dde6282eda16eee8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a252d96105c6dd1721e2",
+    "url": "/static/css/main.5991f105.chunk.css"
+  },
+  {
+    "revision": "6db87eed34d8df91000e",
+    "url": "/static/js/2.50c3a772.chunk.js"
+  },
+  {
+    "revision": "a252d96105c6dd1721e2",
+    "url": "/static/js/main.a381261d.chunk.js"
+  },
+  {
+    "revision": "1ef8de3810017432066c",
+    "url": "/static/js/runtime-main.d29b42d8.js"
+  }
+]);
